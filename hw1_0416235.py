a3d6d3a9553672cec1d4c13f2a8f3ca474cc7d90
AilurusUmbra <contradict9137@gmail.com>
2019-03-07 19:57:04 +0800

    First commit

---
+++ hw1_0416235.py
@@ -0,0 +1,58 @@
+import yaml
+
+
+# Argument Parser
+import argparse
+parser = argparse.ArgumentParser(description='Validating YAML file...')
+parser.add_argument('-i', '--inputFile', type=str, help='input filename', required=True)
+parser.add_argument('-o', '--outputFile', type=str, help='output filename', required=True)
+#parser.add_argument('-q', '--quite', action="store_true", help='quiet mode', default=False)
+args = parser.parse_args()
+
+
+# Read input yaml
+
+inputyaml = yaml.load(open(args.inputFile, "r"))
+
+
+# Validation
+schema = {'EC_Engine': {'populationSize': 100,
+  'generationCount': 50,
+  'randomSeed': 10,
+  'evaluatorType': 'parabola',
+  'jobName': 'test',
+  'scalingParam': 2.5}}
+mandatoryParams = ['populationSize', 'generationCount', 'evaluatorType']
+
+missingParams = []
+incorrectTypes = []
+exceptParams = []
+
+if 'EC_Engine' in inputyaml.keys():
+    # check missing parameters
+    for p in mandatoryParams:
+        if p not in inputyaml['EC_Engine'].keys():
+            missingParams.append(p)
+
+    # check incorrect types
+    try:
+        for p in inputyaml['EC_Engine'].keys():
+            if type(schema['EC_Engine'][p]) != type(inputyaml['EC_Engine'][p]):
+                incorrectTypes.append(p)
+    except:
+        exceptParams.append(p)
+        print("Exception occurs...")
+        print('exception parameter: ', exceptParams)
+
+else:
+    missingParams.append('EC_Engine')
+
+
+# Write validation results
+validation = {'missingParams': sorted(missingParams),
+              'incorrectTypes': sorted(incorrectTypes)}
+with open('rst.yml', 'w') as outfile:
+    yaml.dump(validation, outfile, default_style=False)
+
+#if args.quiet is False:
+#    print(validation)
